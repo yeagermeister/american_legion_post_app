81afb5609713158cea899852a23173f978dfbc3f
yeagermeister <johnyeager75@gmail.com>
2024-02-20 07:52:57 -0500

    working on images

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://post291.org/api';
+const API_URL = 'https://post291.org/';
 
 // User related API calls
 export const getMemberId = (memberId) => {
