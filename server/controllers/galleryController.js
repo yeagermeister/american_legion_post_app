f97170db70022f971405703f2fd282500ae2f754
yeagermeister <johnyeager75@gmail.com>
2024-02-20 11:51:18 -0500

    cross fingers

--- server/controllers/galleryController.js
+++ server/controllers/galleryController.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-const dir = `/var/www/post291.org/images/`;
+const dir = `./testUploads/`;
 
 
 if (!fs.existsSync(dir)){
