77b30de1e8013ac8640ccf4a0093e8aac22a3cf6
yeagermeister <johnyeager75@gmail.com>
2025-07-16 19:18:35 -0400

    moving some stuff around on the home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -20,6 +20,13 @@ const home = () => {
                 </Col>
             </Row>
             <Row>
+                <Col xs={12} lg={4}></Col>
+                <Col xs={12} lg={4}>
+                    <a href="https://post291.org/privacy.html" target="_blank" rel="noopener noreferrer">Privacy Policy</a>
+                </Col>
+                <Col xs={12} lg={4}></Col>
+            </Row>
+                        <Row>
                 <Col xs={12} lg={4}>
                   <a href="https://post291.org/privacy.html" target="_blank" rel="noopener noreferrer">Privacy Policy</a>
                 </Col>
@@ -30,18 +37,12 @@ const home = () => {
                     <p>Have a question or need support on the webpage or the post app?  Contact <a href="mailto:johnyeager75@gmail.com">John Yeager</a></p>
                 </Col>
             </Row>
-        </Container>
-        <Container>
             <Row>
+                <Col xs={12} lg={4}></Col>
                 <Col xs={12} lg={4}>
-                    <h1></h1>
+                    <p>Have a question or need support on the webpage or the post app?  Contact <a href="mailto:johnyeager75@gmail.com">John Yeager</a></p>
                 </Col>
-                <Col xs={12} lg={4}>
-                    <h1></h1>
-                </Col>
-                <Col xs={12} lg={4}>
-                    <h1></h1>
-                </Col>
+                <Col xs={12} lg={4}></Col>
             </Row>
         </Container>
         </>
